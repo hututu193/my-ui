d52ca89d43ae5fb8970cd7fe29aa7fd512485782
hututu193 <116326894+hututu193@users.noreply.github.com>
2025-11-10 01:17:42 +0800

    fix: update package.json configuration

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,18 +1,24 @@
+// 导入样式
+import './gulu.scss'
+
+// 导入组件
 import Switch from './Switch.vue'
 import Select from './Select.vue'
 import Option from './Option.vue'
 import Message from './Message.vue'
 import Input from './Input.vue'
 import Checkbox from './Checkbox.vue'
-import CheckboxGroup from './CheckboxGroup.vue';
+import CheckboxGroup from './CheckboxGroup.vue'
 import Dialog from './Dialog.vue'
 import Tab from './Tab.vue'
 import Tabs from './Tabs.vue'
 import Button from './Button.vue'
+import { openDialog } from './openDialog.ts'
 
+// 导出所有组件和工具函数
 export {
-    Select,
     Switch,
+    Select,
     Option,
     Message,
     Input,
@@ -21,7 +27,6 @@ export {
     Dialog,
     Tab,
     Tabs,
-    Button
+    Button,
+    openDialog
 }
-
-
